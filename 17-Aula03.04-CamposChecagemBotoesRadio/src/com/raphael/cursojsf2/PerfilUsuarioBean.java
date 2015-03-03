4bdea9b83bd2fc98d9379d704ed50d0c1915cf19
raphaelso <raphaelsoliveira@gmail.com>
2015-03-02 22:32:57 -0300

    First commit class 03.04 of course jsf2

---
+++ 17-Aula03.04-CamposChecagemBotoesRadio/src/com/raphael/cursojsf2/PerfilUsuarioBean.java
@@ -0,0 +1,39 @@
+package com.raphael.cursojsf2;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class PerfilUsuarioBean {
+	
+	private String sexo;
+	private boolean receberNovidades;
+	private String[] linguagensFavoritas;
+
+	public String[] getLinguagensFavoritas() {
+		return linguagensFavoritas;
+	}
+
+	public void setLinguagensFavoritas(String[] linguagensFavoritas) {
+		this.linguagensFavoritas = linguagensFavoritas;
+	}
+
+	public String enviar() {
+		return "Confirmacao";
+	}
+	
+	public String getSexo(){
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public boolean getReceberNovidades(){
+		return receberNovidades;
+	}
+	
+	public void setReceberNovidades(boolean receberNovidades){
+		this.receberNovidades = receberNovidades;
+	}
+}
